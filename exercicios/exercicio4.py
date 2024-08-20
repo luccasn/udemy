f1b2bd2bf078dfd1668e7c873923f3d6aa011950
luccas neves <luccas_neves@hotmail.com>
2024-08-20 15:40:08 -0300

    atualizar commit

--- exercicios/exercicio4.py
+++ exercicios/exercicio4.py
@@ -3,11 +3,12 @@
 
 """
 num: int = int (input('Informe um número: ')) #num recebe = numero inteiro input mostra ao ususario
-num2: int = int (input('Informe um outro número'))
+num2: int = int (input('Informe um outro número: '))
 
 if num > num2:
-    print(f'o primeiro número{num} é maior que {num2}')
+    print(f'o primeiro número {num} é maior que {num2}')
 elif num == num2:
     print(f'Os números {num} e {num2} são iguais.')
 else:
     print(f'O segundo número {num2} é maior que {num}')
+
