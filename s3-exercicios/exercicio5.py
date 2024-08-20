539c18b5d63b2bd0c996204ab7f7f5cad199e810
luccas neves <luccas_neves@hotmail.com>
2024-08-20 15:18:11 -0300

    outros exercicios das aulas da udemy

---
+++ s3-exercicios/exercicio5.py
@@ -0,0 +1,11 @@
+"""2. Faça um programa que leia um número inteiro fornecido pelo usuário.
+Se esse número for positivo, calcule a raiz quadrada do número e apresente-a.
+Se o número for negativo, mostre uma mensagem dizendo que o número é inválido."""
+
+from math import sqrt # from math import serve para importar a biblioteca para que possa fazer operações matematicas
+
+num: int = int(input('Digite um número inteiro: '))
+if num > 0:
+    print(f'A raiz quadrada {num} é {sqrt(num)}')
+else:
+    print(f'O número {num} é inválido')
---
+++ s3-exercicios/exercicio6.py
@@ -0,0 +1,8 @@
+"""3. Faça um programa que recebe um número inteiro
+e informe se este número é par ou ímpar."""
+
+num: int = int(input('Digite um número: '))
+if num % 2 == 0: #o resto da divisão de um número por 2 vai ser igual a 0 caso ele seja par
+    print(f'Este número {num} é par')
+else:
+    print(f'Este número {num} é impar')
